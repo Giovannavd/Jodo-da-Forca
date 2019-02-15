c84787caab267dd4ecac9aef1459dd09a94a8927
guilherme-amarante <guilhermeamarante@hotmail.com>
2019-02-15 19:57:52 -0200

    inicio html,CSS e JS

---
+++ script.js
@@ -0,0 +1,27 @@
+let palavras = [`girassol`,`maracujá`,`palmeira`,`melancia`,`mandioca`,`guepardo`,`avestruz`,`elefante`];
+
+let aleatorio = function (min, max){
+    return Math.floor(Math.random() * (max - min)) - min;
+}
+
+let respostas = palavras [aleatorio(0,palavras.length -1)];
+
+prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`);
+
+let i = 0;
+
+while (i <= 5){
+    
+    if(palavras.includes == resposta){
+        alert("Você está certo!")
+
+        while 
+    }
+
+    else{
+        alert("Você está errado");
+        prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`)
+    }
+
+    i++
+}

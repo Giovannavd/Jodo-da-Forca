bb660362e71374bcec4e4f8e0fb50cb8213c3ead
guilherme-amarante <guilhermeamarante@hotmail.com>
2019-02-18 17:08:25 -0300

    atualizações JS

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+
 let palavras = [`girassol`, `maracujá`, `palmeira`, `melancia`, `mandioca`, `guepardo`, `avestruz`, `elefante`];
 
 let aleatorio = function (min, max) {
     return Math.floor(Math.random() * (max - min)) - min;
 }
 
-let respostas = palavras[aleatorio(0, palavras.length - 1)];
-
-prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`);
+let resposta = palavras[aleatorio(0, palavras.length - 1)];
+let palpite = prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`); 
 
 let i = 0;
 
-while (i <= 5) {
+while (i <= 6) {
+    console.log(resposta);
+    console.log(palpite);
+    console.log(resposta.includes(palpite));
+    if (resposta.includes(palpite)) {
+        alert("Você está certo!");
 
-    if (palavras.includes == resposta) {
-        alert("Você está certo!")
 
 
     }
-
     else {
         alert("Você está errado");
-        prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`)
+        i++;
     }
-
-    i++
+    palpite = prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`);
+    
 }
+
+
+let input = document.querySelector("input")
+
+// let input
+
+// XPathEvaluator.value 

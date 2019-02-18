057cecdf45b01f6a343ba48c4db9ec931a53f706
guilherme-amarante <guilhermeamarante@hotmail.com>
2019-02-17 21:06:58 -0300

    aperfeiçoamento CSS

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let palavras = [`girassol`,`maracujá`,`palmeira`,`melancia`,`mandioca`,`guepardo`,`avestruz`,`elefante`];
+let palavras = [`girassol`, `maracujá`, `palmeira`, `melancia`, `mandioca`, `guepardo`, `avestruz`, `elefante`];
 
-let aleatorio = function (min, max){
+let aleatorio = function (min, max) {
     return Math.floor(Math.random() * (max - min)) - min;
 }
 
-let respostas = palavras [aleatorio(0,palavras.length -1)];
+let respostas = palavras[aleatorio(0, palavras.length - 1)];
 
 prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`);
 
 let i = 0;
 
-while (i <= 5){
-    
-    if(palavras.includes == resposta){
+while (i <= 5) {
+
+    if (palavras.includes == resposta) {
         alert("Você está certo!")
 
-        while 
+
     }
 
-    else{
+    else {
         alert("Você está errado");
         prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`)
     }

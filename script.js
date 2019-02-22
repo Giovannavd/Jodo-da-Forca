d2578a38760d7d41553d5274aec369460b8c7aa6
Giovanna <giovannavd@gmail.com>
2019-02-22 16:39:37 -0300

    Acertar o codigo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 let palavras = [`girassol`, `maracujá`, `palmeira`, `melancia`, `mandioca`, `guepardo`, `avestruz`, `elefante`];
+let resposta = palavras[aleatorio(0, palavras.length - 1)];
 let input = document.querySelector('#palpite');
 let button = document.querySelector('#button');
 let listaVazia = '';
-let palavrasLista = palavras[0].split('');
+let palavrasLista = resposta.split('');
 let posicoesDaLetra = [];
 let bracod = document.querySelector ('#bracod');
 let cabeca = document.querySelector('#cabeca');
@@ -11,67 +12,43 @@ let tronco = document.querySelector('#tronco');
 let bracoe = document.querySelector('#bracoe');
 let pernad = document.querySelector('#pernad');
 let pernae = document.querySelector('#pernae');
-let corpo = [bracod, tronco, bracoe, pernad, pernae, cabeca];
+let corpo = [tronco, bracod, bracoe, pernad, pernae, cabeca];
+let chances = 0;
+let espaços = document.querySelectorAll('span');
+let indicePalavra = percorrerPalavra();
 
-
-button.onclick = function(){
-    listaVazia = input.value;
-    input.value = '';
+let aleatorio = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) - min;
 }
 
 const percorrerPalavra = function(){
     for(i = 0; i < palavrasLista.length; i++){
-        if(listaVazia == palavrasLista[i]){
-            posicoesDaLetra.push(i);
-        }   
-
-
-        else{
-            for(i = 0; i <= corpo.length; i++){
-               console.log
-            }
-
-
-        }
-    }
-}
+}}
 
 let span = document.querySelectorAll('span');
 for (i in palavras.length){
     span[i] = input;
 }
 
+button.onclick = function(){
+    listaVazia = input.value;
+    input.value = '';
+}
+// 
+while (chances <= 5){
+    chances++;
+    if(resposta.includes(input)){
+        indicePalavra.indexOf(input)
+        [indice].innerHTML = input
+    }else{
+        //colocar aparecer o parte do  corpo
+        corpo[chances].style.display = 'block';
+        chances ++;
+    }
 
-
-
-
-
-// let aleatorio = function (min, max) {
-//     return Math.floor(Math.random() * (max - min)) - min;
-// }
-
-// let resposta = palavras[aleatorio(0, palavras.length - 1)];
-// let palpite = prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`); 
-
-// let i = 0;
-
-// while (i <= 6) {
-//     console.log(resposta);
-//     console.log(palpite);
-//     console.log(resposta.includes(palpite));
-//     if (resposta.includes(palpite)) {
-//         alert("Você está certo!");
-
-
-
-//     }
-//     else {
-//         alert("Você está errado");
-//         i++;
-//     }
-//     palpite = prompt(`A palavra tem 8 letras, e o tema é Natureza!${resposta}`);
+}
     
-// }
 
 
-// let input = document.querySelector("input")
+
+
